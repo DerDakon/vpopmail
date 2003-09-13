66d24365c54fb6df60b332ced530ab14fdc16aee
Tom Collins <tom@tomlogic.com>
2003-09-13 00:23:15 +0000

    remove vpopmail.config.sh from repository
    git-svn-id: https://svn.code.sf.net/p/vpopmail/code/trunk@13 8f0cf6e2-dddb-4e5e-b901-7f5e1d474549

--- vpopmail/vldap.c
+++ vpopmail/vldap.c
@@ -284,15 +284,11 @@ struct vqpasswd *vauth_getpw(char *user, char *domain)
    ldap_value_free(vals);
   } 
 #endif
-  /* this is necessary to enforce the qmailadmin-limits
-     a gid_mask is created from the qmailadmin-limits, which is then ORed againt the users gid field,
-     unless the user has the V_OVERRIDE flag set
-  */
-  if (vget_limits (domain,&limits) == 0) {
-  	if (! vpw->pw_gid && V_OVERRIDE) {
-  		vpw->pw_gid |= vlimits_get_gid_mask (&limits);
-  	}
-  }
+
+  if ((! pwent.pw_gid && V_OVERRIDE)
+    && (vget_limits (in_domain, &limits) == 0) {
+      pwent.pw_flags = pwent.pw_gid | vlimits_get_gid_mask (&limits);
+  } else pwent.pw_flags = pwent.pw_gid;
 
  return vpw;
 
