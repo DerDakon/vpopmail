564e91c344edeb9de87c73cbca4692c9636791d4
Michael Bowe <mbowe@pipeline.com.au>
2004-04-17 21:51:40 +0000

    fix for bug in roaming users where if auth was not SQL, and user's IP was not available, then open-smtp.tmp.pid would be created for each login and never removed.
    git-svn-id: https://svn.code.sf.net/p/vpopmail/code/trunk@296 8f0cf6e2-dddb-4e5e-b901-7f5e1d474549

--- vpopmail/vpopmail.c
+++ vpopmail/vpopmail.c
@@ -1,5 +1,5 @@
 /*
- * $Id: vpopmail.c,v 1.33 2004-04-07 14:30:40 kbo Exp $
+ * $Id: vpopmail.c,v 1.34 2004-04-17 21:51:40 mbowe Exp $
  * Copyright (C) 2000-2004 Inter7 Internet Technologies, Inc.
  *
  * This program is free software; you can redistribute it and/or modify
@@ -2462,6 +2462,11 @@ int open_smtp_relay()
 
   mytime = time(NULL);
 
+  ipaddr = get_remote_ip();
+  if ( ipaddr == NULL ) {
+      return 0;
+  }
+
 #ifdef FILE_LOCKING
   /* by default the OPEN_SMTP_LOK_FILE is ~vpopmail/etc/open-smtp.lock */
   if ( (fd_lok_file=open(OPEN_SMTP_LOK_FILE, O_WRONLY | O_CREAT))<0) return(-1);
@@ -2481,10 +2486,10 @@ int open_smtp_relay()
     }
   }
 
-  /* by default the OPEN_SMTP_TMP_FILE is ~vpopmail/etc/open-smtp.tmp */
+  /* by default the OPEN_SMTP_TMP_FILE is ~vpopmail/etc/open-smtp.tmp.pid */
   snprintf(open_smtp_tmp_filename, sizeof(open_smtp_tmp_filename),
            "%s.%lu", OPEN_SMTP_TMP_FILE, (long unsigned)getpid());
-  /* create the tmp file as open-smtp.tmp.pid */
+  /* create the tmp file */
   fs_tmp_file = fopen(open_smtp_tmp_filename, "w+");
 
   if ( fs_tmp_file == NULL ) {
@@ -2493,17 +2498,6 @@ int open_smtp_relay()
     close(fd_lok_file);
 #endif /* FILE_LOCKING */
     /* failed to create the tmp file */
-    return(-1);
-  }
-
-  ipaddr = get_remote_ip();
-
-  if ( ipaddr == NULL ) {     
-#ifdef FILE_LOCKING
-    unlock_lock(fd_lok_file, 0, SEEK_SET, 0);
-    close(fd_lok_file);
-#endif /* FILE_LOCKING */
-    /* failed to get user's ip address */
     return(-1);
   }
 
