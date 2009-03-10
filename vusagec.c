8e7411d45177485124d4155d71184d4e56c0f301
Matt Brookings <matt@inter7.com>
2009-03-10 14:55:08 +0000

    Added vpopmail usage daemon client API to libvpopmail Fixed some issues with backfill compiling in correctly
    git-svn-id: https://svn.code.sf.net/p/vpopmail/code/trunk@692 8f0cf6e2-dddb-4e5e-b901-7f5e1d474549

---
+++ vusagec.c
@@ -0,0 +1,76 @@
+/*
+   $Id$
+
+   * Copyright (C) 2009 Inter7 Internet Technologies, Inc.
+   *
+   * This program is free software; you can redistribute it and/or modify
+   * it under the terms of the GNU General Public License as published by
+   * the Free Software Foundation; either version 2 of the License, or
+   * (at your option) any later version.
+   *
+   * This program is distributed in the hope that it will be useful,
+   * but WITHOUT ANY WARRANTY; without even the implied warranty of
+   * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   * GNU General Public License for more details.
+   *
+   * You should have received a copy of the GNU General Public License
+   * along with this program; if not, write to the Free Software
+   * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include "client.h"
+#include "storage.h"
+
+int main(int argc, char *argv[])
+{
+#ifdef QUICK_QUERY
+   int ret = 0, i = 0;
+#else
+   void *handle = NULL;
+   int i = 0, ret = 0;
+#endif
+   storage_t uusage = 0, dusage = 0;
+
+   if (argc < 2) {
+	  printf("Usage: %s <email> [...]\n", argv[0]);
+	  return 1;
+   }
+
+#ifdef QUICK_QUERY
+   for (i = 1; i < argc; i++) {
+	  ret = client_query_quick(argv[i], &uusage, &dusage);
+	  if (!ret)
+		 printf("client_query_quick failed\n");
+	  else {
+		 if (uusage == -1)
+			printf("No such user: %s\n", argv[i]);
+		 else
+			printf("%s: %llu byte(s), domain: %llu byte(s)\n", argv[i], uusage, dusage);
+	  }
+   }
+#else
+   handle = client_connect();
+   if (handle == NULL) {
+	  printf("client_connect failed\n");
+	  return 1;
+   }
+
+   for (i = 1; i < argc; i++) {
+	  ret = client_query(handle, argv[i], strlen(argv[i]), &uusage, &dusage);
+	  if (!ret) {
+		 printf("client_query failed\n");
+		 continue;
+	  }
+
+	  if (uusage == -1)
+		 printf("No such user: %s\n", argv[i]);
+	  else
+		 printf("%s: %llu byte(s), domain: %llu byte(s)\n", argv[i], uusage, dusage);
+   }
+
+   client_close(handle);
+#endif
+   return 0;
+}
