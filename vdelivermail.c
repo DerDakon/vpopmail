0f560c1a02dded428c46c7d9c59ff8533859c872
Rolf Eike Beer <eike@sf-mail.de>
2014-08-21 17:34:27 +0200

    vdelivermail: add static

--- vdelivermail.c
+++ vdelivermail.c
@@ -322,7 +322,7 @@ int process_valias(void)
 #endif
 
 /* Forks off qmail-inject.  Returns PID of child, or 0 for failure. */
-pid_t qmail_inject_open(char *address)
+static pid_t qmail_inject_open(char *address)
 {
  int pim[2];
  pid_t pid;
@@ -351,7 +351,7 @@ pid_t qmail_inject_open(char *address)
     return(pid);
 }
 
-int fdcopy (int write_fd, int read_fd, const char *extra_headers, size_t headerlen, char *address)
+static int fdcopy (int write_fd, int read_fd, const char *extra_headers, size_t headerlen, char *address)
 {
   char msgbuf[4096];
   ssize_t file_count;
